906bb77dc0efe190cb88966b5ef6ba6696434a31
angelinalaranjinha <00001099573075sp@al.educacao.sp.gov.br>
2024-08-01 20:26:05 -0300

    adicionando arquivos js e cs

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let cor;
+let posicaohorzintal; // X
+let posicaoVertical; // X
+let posicaoHorizontal2; // x
+let posicaoVertical2; // x
+function setup() {
+  createCanvas(400, 400);
+  background(color(100, 0 , 0));
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 0;
+  posicaoVertical = random(height);
+}  
+  
